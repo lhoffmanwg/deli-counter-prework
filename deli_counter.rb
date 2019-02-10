bbf1d81a4503aa39d3b451fd4f1a6399a4c1be29
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-10 23:26:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ end
 def now_serving(katz_deli)
   if katz_deli = []
    puts "There is nobody waiting to be served!"
-  #else 
+  else 
     name = katz_deli.first 
     puts "Currently serving #{name}."
     katz_deli.shift 
