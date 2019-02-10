fc8b8193c664c35391f170bad042065bb1bf6b09
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-10 23:10:49 +0000

    Automatically backed up by Learn

--- spec/deli_counter_spec.rb
+++ spec/deli_counter_spec.rb
@@ -7,7 +7,6 @@ describe 'Deli Counter' do
     context "there is nobody in line" do
       it "should say the line is empty" do
         # This line checks the current standard output (your terminal screen)
-        # to make sure the correct output has been puts'ed.
         expect($stdout).to receive(:puts).with("The line is currently empty.")
         line(katz_deli)
       end
